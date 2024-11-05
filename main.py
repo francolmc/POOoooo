4996b3b6adeb57a50f1c6e91d7acf2f7abc0bdc9
francolmc <franco.morales@outlook.com>
2024-11-05 20:55:23 -0300

    feat: Como crear clase e instanciar

---
+++ main.py
@@ -0,0 +1,10 @@
+# Clases en python
+class Product:
+    # Atributo name
+    name = "Sillas"
+
+# Instanciar clase Product
+product = Product()
+
+# Acceder al atributo name del objeto product
+print(product.name)
