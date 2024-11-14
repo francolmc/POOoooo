7083368b561716db96c8ec8fc398171401a5f179
francolmc <franco.morales@outlook.com>
2024-11-13 21:59:20 -0300

    feat: manejo de seerder para productos

--- Clase 8 Tarea SC/main.py
+++ Clase 8 Tarea SC/main.py
@@ -1,20 +1,6 @@
 from entities.product import Product
 from entities.shopping_cart import ShoppingCart
-
-polo = Product("Polo")
-polo.set_id(1)
-polo.add_stock(4)
-polo.set_price(15000)
-
-celular = Product("Celular")
-polo.set_id(2)
-celular.add_stock(10)
-celular.set_price(400000)
-
-zapatilla = Product("Zapatilla")
-polo.set_id(3)
-zapatilla.add_stock(2)
-zapatilla.set_price(95000)
+from utils.seeder import zapatilla, polo, celular
 
 carrito = ShoppingCart()
 carrito.set_id(1)
---
+++ Clase 8 Tarea SC/utils/seeder.py
@@ -0,0 +1,16 @@
+from entities.product import Product
+
+polo = Product("Polo")
+polo.set_id(1)
+polo.add_stock(4)
+polo.set_price(15000)
+
+celular = Product("Celular")
+celular.set_id(2)
+celular.add_stock(10)
+celular.set_price(400000)
+
+zapatilla = Product("Zapatilla")
+zapatilla.set_id(3)
+zapatilla.add_stock(2)
+zapatilla.set_price(95000)
