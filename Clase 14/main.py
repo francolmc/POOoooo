65d586b8617ef5895e496b33f2b67a5e17faeddb
francolmc <franco.morales@outlook.com>
2024-12-03 20:49:42 -0300

    feat: serializacion y deserializacion de json

--- Clase 14/main.py
+++ Clase 14/main.py
@@ -1,11 +1,13 @@
 import requests
 from model.libro import Libro
 
-libro = Libro("prueba", ["prueba"], "prueba", "123-123", 123, "pruebita")
+# Serealizar la instancia a JSON
+libro = Libro("Hans Topo", ["psicologia", "autoayuda"], "Explica como comer una naranja en este mundo tan cruel.", "123-123", 123, "Como comer una naranja sin poner caras")
 print(libro.to_json())
 
+# Deserializacion de un JSON a una instancia
 response = requests.get("https://poo.nsideas.cl/api/libros/978-3-16-148410-0")
-
 if response.status_code == 200:
-    data = response.json()
-    print(data)
+    data = response.text
+    libro2 = Libro.from_json(data)
+    print(f"Libro: {libro2.titulo} | Autor: {libro2.autor}")
--- Clase 14/model/libro.py
+++ Clase 14/model/libro.py
@@ -1,15 +1,18 @@
 import json
 
 class Libro:
-    def __init__(self, autor: str, categorias: list, descripcion: str, isbn: str, nro_paginas: int, titulo: str) -> None:
+    def __init__(self, autor: str, categorias: list, descripcion: str, isbn: str, numero_paginas: int, titulo: str) -> None:
         self.autor = autor
         self.categorias = categorias
         self.descripcion = descripcion
         self.isbn = isbn
-        self.nro_paginas = nro_paginas
+        self.numero_paginas = numero_paginas
         self.titulo = titulo
 
-    def to_json(self):
+    def to_json(self) -> str:
         return json.dumps(self, default=lambda o: o.__dict__, indent=4)
     
-    
+    @staticmethod # Nos permite ejecutar el metodo sin la necesidad de instanciar
+    def from_json(data) -> 'Libro':
+        data_dict = json.loads(data)
+        return Libro(**data_dict)
